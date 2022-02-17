547ef45659aff29dbaefc4c05c59404b23c8c40e
Bukakow <bookovskytsfh@gmail.com>
2022-02-17 13:43:29 +0100

    Refactor and tweak PickArtistGUI

--- src/main/java/me/nefelion/spotifynotifier/gui/PickArtistGUI.java
+++ src/main/java/me/nefelion/spotifynotifier/gui/PickArtistGUI.java
@@ -6,21 +6,41 @@ import java.util.List;
 
 public class PickArtistGUI extends PopupGUI {
 
-    JList<String> jList;
-    int selectedIndex = -1;
+    private final List<String> artistList;
+    private int selectedIndex = -1;
 
     public PickArtistGUI(List<String> artistList) {
+        this.artistList = artistList;
+        createGUI();
+    }
+
+    private void createGUI() {
+        dialog.setMinimumSize(new Dimension(180, 0));
+        dialog.setMaximumSize(new Dimension(500,500));
+        dialog.add(getTrackListScrollPane(getInitialList(artistList)));
+        dialog.pack();
         setTitle("Select an artist");
+    }
+
+    private JList<String> getInitialList(List<String> artistList) {
+        final JList<String> jList;
         jList = new JList<>(artistList.toArray(new String[0]));
         jList.addListSelectionListener(e -> {
             selectedIndex = jList.getSelectedIndex();
             dialog.dispose();
         });
-        dialog.setMinimumSize(new Dimension(180, 0));
-        dialog.add(jList);
-
-        dialog.pack();
+        return jList;
     }
+
+    private JScrollPane getTrackListScrollPane(JList<String> list) {
+        JScrollPane scrollTrackList = new JScrollPane(list);
+        scrollTrackList.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED);
+        scrollTrackList.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+        scrollTrackList.getVerticalScrollBar().setUnitIncrement(16);
+        //scrollTrackList.setPreferredSize(new Dimension(300, 300));
+        return scrollTrackList;
+    }
+
 
     public int getPickedIndex() {
         return selectedIndex;
